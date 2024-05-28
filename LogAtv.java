838261d1258c5aaefceeaeee7e558f4e8c009cb6
Tommysfp <164034156+Tommysfp@users.noreply.github.com>
2024-05-28 17:52:23 -0300

    Update LogAtv.java

--- LogAtv.java
+++ LogAtv.java
@@ -1,28 +1,28 @@
+import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class LogAtv {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Faça uma divizao");
+        Scanner Scanner = new Scanner(System.in);
         FileWriter writer = null;
-        try {
-            writer = new FileWriter("output.txt");
+        writer = new FileWriter("output2.txt");
 
-            writer.write("Olá, mundo!");
-            
-            System.out.println("Mensagem escrita no arquivo com sucesso.");
-        } catch (IOException e) {
-            
-            System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+        System.out.println("Digite o primeiro número: ");
+        int num1 = Scanner.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int num2 = Scanner.nextInt();
+        try {
+            int resultado = num1 / num2;
+            System.out.println("O resultado da divisão é: " + resultado);
+        } catch (ArithmeticException e) {
+            writer.write("Erro ao dividir por zero: " + e.getMessage());
         } finally {
-            try {
-                
-                if (writer != null) {
-                    writer.close();
-                }
-            } catch (IOException e) {
-               
-                System.err.println("Erro ao fechar o arquivo: " + e.getMessage());
-            }
+            System.out.println("Operação finalizada.");
         }
+        writer.close();
+        Scanner.close();
     }
+
 }
